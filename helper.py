109e715741feb6ab27d328be8039fbd53607b43d
Asa Mohammed <asa.mohammed22@gmail.com>
2023-09-11 21:58:19 -0400

    adds like, dislike, sniped

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 import responses
+import dbmanager
 
 async def process_msg(msg):
     # Get message text
@@ -9,3 +10,39 @@ async def process_msg(msg):
     
     elif text.startswith('!tomatoes'):
         await responses.post_tomatos_command(msg)
+
+    elif text.startswith('!like'):
+        if not msg.mentions:
+            return
+    
+        elif bool(msg.mentions[0].bot):
+            await msg.channel.send('**LEAVE US ALONE!!!** 😡')
+
+        else:
+            # Get first mention in list
+            target_user = msg.mentions[0].id
+            dbmanager.add_likes(target_user, 1)
+
+    elif text.startswith('!dislike'):
+        if not msg.mentions:
+            return
+
+        elif bool(msg.mentions[0].bot):
+            await msg.channel.send('**LEAVE US ALONE!!!** 😡')
+
+        else:
+            # Get first mention in list
+            target_user = msg.mentions[0].id
+            dbmanager.add_dislikes(target_user, 1)
+
+    elif text.startswith('!sniped'):
+        if not msg.mentions:
+            return
+
+        elif bool(msg.mentions[0].bot):
+            await msg.channel.send('**LEAVE US ALONE!!!** 😡')
+
+        else:
+            for target_user in msg.mentions:
+                dbmanager.add_sniped(target_user, 1)
+                dbmanager.add_kills(msg.author.id, 1)
